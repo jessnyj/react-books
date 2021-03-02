a98cf2b006b4f8da464a537bffe3d148f0b73e01
jessny <jessnyjoseph@gmail.com>
2021-03-02 15:51:17 -0600

    added image and content container within search card

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -1,11 +1,51 @@
 import React from "react";
 import "./card.css";
 
-function Card({children}) {
+function Card({ children }) {
   return (
-      <div>
-        <h1 className="resultsSearch">Results:</h1>
+    <div>
+      <h1 className="resultsSearch">Results:</h1>
+      <div className="card">
+        <div className="card-content">
+          <div className="content">
+            <h4>Book Title</h4>
+            <h5>Authors</h5>
+            <article className="media">
+              <figure className="media-left">
+                <p className="image is-64x64">
+                  <img src="https://bulma.io/images/placeholders/128x128.png"></img>
+                </p>
+              </figure>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                    {"\n"}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+                </p>
+                </div>
+                <nav className="level is-mobile">
+                  <div className="level-left">
+                    <a className="level-item">
+                      <span className="icon is-small"><i className="fas fa-reply"></i></span>
+                    </a>
+                    <a className="level-item">
+                      <span className="icon is-small"><i className="fas fa-retweet"></i></span>
+                    </a>
+                    <a className="level-item">
+                      <span className="icon is-small"><i className="fas fa-heart"></i></span>
+                    </a>
+                  </div>
+                </nav>
+              </div>
+              {/* <div className="media-right">
+                <button className="delete"></button>
+              </div> */}
+            </article>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
 
