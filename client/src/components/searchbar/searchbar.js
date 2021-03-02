73c5815ba931735fc6dad24042d13e2d40be7e8c
jessny <jessnyjoseph@gmail.com>
2021-03-02 15:20:50 -0600

    added margins and font changes to search box

--- client/src/components/searchbar/searchbar.js
+++ client/src/components/searchbar/searchbar.js
@@ -5,13 +5,13 @@ function Searchbar() {
 	return (
 		<section>
 			<div className="card">
-				<div className="card-content">
+				<div className="card-content searchTitle">
 					Book Search
 				<div className="content">
-					<label className="label">Book</label>
-					<input className="input is-link" type="text" placeholder="Link input" />
-					<button className="button is-link">Normal</button>
-	  		</div>
+						<label className="label bookLabel">Book:</label>
+						<input className="input is-link" type="text" placeholder="Link input" />
+						<button className="button is-link searchBtn">Normal</button>
+					</div>
 				</div>
 			</div>
 		</section>
