912d3371904de4cd8be5df510ca7722fae80cf7f
jessny <jessnyjoseph@gmail.com>
2021-03-02 15:27:41 -0600

    added container to containerjs

--- client/src/components/container/container.js
+++ client/src/components/container/container.js
@@ -3,13 +3,11 @@ import Card from "../card/card";
 
 function Container() {
   return (
-    <section className="section is-large">
-      <h1 className="title">Results</h1>
-      <h2 className="subtitle">
-  </h2>
-  <Card />
-
-    </section>
+    <div className="container is-widescreen">
+      <div className="notification is-link">
+        <Card />
+      </div>
+    </div>
 
   );
 }
