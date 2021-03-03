eedc048a84d48e612e491825b676c922249922e9
Muhammad K <akhalid.code@gmail.com>
2021-03-02 20:59:31 -0800

    add search functionality

--- client/src/components/searchcard/searchcard.js
+++ client/src/components/searchcard/searchcard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './searchcard.css';
 
-function SearchCard() {
+function SearchCard(props) {
+	console.log(props);
 	return (
 		<div>
-
+			<p>{props.title}</p>
+			<p>{props.author}</p>
+			<p>{props.description}</p>
+			<p>{props.image}</p>
+			<br></br>
+			{/* <p>{props.link}</p> */}
 		</div>
 	);
 };
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,16 +1,10 @@
 import React from "react";
 import SearchContainer from "../components/searchcontainer/searchcontainer";
-// import Card from "../components/card/card";
-import Searchbar from "../components/searchbar/searchbar";
 
 function Search() {
 	return (
 		<div className="container">
-			<Searchbar />
-			<SearchContainer>
-			{/* map over card array */}
-			
-			</SearchContainer>
+			<SearchContainer />
 		</div>
 	);
 };
